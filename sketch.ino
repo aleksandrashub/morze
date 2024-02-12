b892dd4832be11237f8c6ae7fcb2016cc8b933f0
aleksandrashub <115869636+aleksandrashub@users.noreply.github.com>
2024-02-12 15:04:15 +0300

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,80 @@
+ int ledPin = 8;
+int dotDelay = 200;
+
+void setup() {
+
+  pinMode(ledPin, OUTPUT);
+
+  // put your setup code here, to run once:
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   //s
+ digitalWrite(ledPin, HIGH);
+  delay(dotDelay);
+  digitalWrite(ledPin, LOW);
+  delay(dotDelay);
+
+
+
+digitalWrite(ledPin, HIGH);
+  delay(dotDelay);
+  digitalWrite(ledPin, LOW);
+  delay(dotDelay);
+  
+
+
+ digitalWrite(ledPin, HIGH);
+  delay(dotDelay);
+  digitalWrite(ledPin, LOW);
+
+
+  delay(3*dotDelay);
+
+
+  //o
+  digitalWrite(ledPin, HIGH);
+  delay(3*dotDelay);
+  digitalWrite(ledPin, LOW);
+  delay(dotDelay);
+
+  digitalWrite(ledPin, HIGH);
+  delay(3*dotDelay);
+  digitalWrite(ledPin, LOW);
+  delay(dotDelay);
+
+  digitalWrite(ledPin, HIGH);
+  delay(3*dotDelay);
+  digitalWrite(ledPin, LOW);
+
+
+
+  delay(3*dotDelay);
+
+//s
+digitalWrite(ledPin, HIGH);
+  delay(dotDelay);
+  digitalWrite(ledPin, LOW);
+  delay(dotDelay);
+
+
+
+digitalWrite(ledPin, HIGH);
+  delay(dotDelay);
+  digitalWrite(ledPin, LOW);
+  delay(dotDelay);
+  
+
+
+ digitalWrite(ledPin, HIGH);
+  delay(dotDelay);
+  digitalWrite(ledPin, LOW);
+
+
+  delay(3*dotDelay);
+
+
+}
